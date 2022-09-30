d980d13ff5b65c121b5a8bb746247fd6271f7592
ccmmaa1030 <ccmmaa1030@gmail.com>
2022-09-30 11:05:54 +0900

    new

--- movie/urls.py
+++ movie/urls.py
@@ -5,7 +5,7 @@ app_name = "movie"
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('detail/', views.detail, name='detail'),
+    # path('detail/', views.detail, name='detail'),
     path('new/', views.new, name='new'),
-    path('edit/', views.edit, name='edit'),
+    # path('edit/', views.edit, name='edit'),
 ]
--- movie/views.py
+++ movie/views.py
@@ -4,4 +4,12 @@ from .models import Review
 # Create your views here.
 
 def index(request):
-    return redirect('movie:index')
+    reviews = Review.objects.all()
+    
+    context = {
+        "reviews": reviews,
+    }
+    return render(request, 'movie/index.html', context)
+
+def new(request):
+    return render(request, 'movie/new.html')
